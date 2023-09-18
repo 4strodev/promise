577ca5ede120dc441fe5666baf71f4091f1396e2
4strodev <jumarurex@gmail.com>
2023-09-18 21:23:10 +0200

    feat:idk

--- utils.go
+++ utils.go
@@ -1,5 +1,24 @@
 package promise
 
-func AwaitAll[T any](promises ...[]Promise[T]){
+import (
+	"context"
+	"sync"
+)
+
+func AwaitAll[T any](ctx context.Context, promises ...Promise[T]) ([]T, error) {
+	mutex := sync.Mutex{}
+	resolvedValues := make([]T, len(promises))
+	for _, promise := range promises {
+		// TODO await with context
+		go func(promise Promise[T]) {
+			mutex.Lock()
+			value, err := promise.Await(ctx)
+			if err != nil {
 	
+			}
+			resolvedValues = append(resolvedValues, value)
+		}(promise)
+	}
+
+	return resolvedValues, nil
 }
